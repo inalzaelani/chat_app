45f3abea0c1b90ccce5c071169432fdcf9f4048e
inalz <inalzaelani2806@gmail.com>
2024-05-16 11:40:42 +0700

    send otp

--- app/src/main/java/com/example/appchat/LoginOTPActivity.java
+++ app/src/main/java/com/example/appchat/LoginOTPActivity.java
@@ -1,18 +1,39 @@
 package com.example.appchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.appchat.utils.AndroidUtil;
 import com.google.firebase.Firebase;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class LoginOTPActivity extends AppCompatActivity {
     String phoneNumber;
+    String verificationCode;
+    PhoneAuthProvider.ForceResendingToken resendingToken;
+    Long timeOut=60L;
+    EditText otpInput;
+    Button nextBtn;
+    ProgressBar progressBar;
+    TextView resendOTPtv;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +41,63 @@ public class LoginOTPActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login_otpactivity);
 
         phoneNumber = getIntent().getExtras().getString("phone");
-        Toast.makeText(this, phoneNumber, Toast.LENGTH_LONG).show();
+        phoneNumber = "+"+phoneNumber;
+        initViews();
+        sendOTP(phoneNumber,false);
+    }
 
-        Map<String,String> data = new HashMap<>();
-        FirebaseFirestore.getInstance().collection("test").add(data);
+    void initViews() {
+        otpInput = findViewById(R.id.login_otp_et);
+        nextBtn = findViewById(R.id.next_otp_btn);
+        progressBar = findViewById(R.id.enter_otp_progress_bar);
+        resendOTPtv = findViewById(R.id.resend_otp_tv);
+    }
+
+    void sendOTP(String phoneNumber,boolean isResend){
+        setInProgress(true);
+        PhoneAuthOptions.Builder builder = PhoneAuthOptions.newBuilder(firebaseAuth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(timeOut, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        signIn(phoneAuthCredential);
+                        setInProgress(false);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        AndroidUtil.showToast(getApplicationContext(),"OTP Verification Failed");
+                        setInProgress(false);
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(s, forceResendingToken);
+                        verificationCode = s;
+                        resendingToken = forceResendingToken;
+                        AndroidUtil.showToast(getApplicationContext(),"OTP Send Successfully");
+                        setInProgress(false);
+                    }
+                });
+        if(isResend){
+            PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
+        } else {
+            PhoneAuthProvider.verifyPhoneNumber(builder.build());
+        }
+    }
+
+    private void signIn(PhoneAuthCredential phoneAuthCredential) {
+    }
+
+    void setInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            nextBtn.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            nextBtn.setVisibility(View.VISIBLE);
+        }
     }
 }
